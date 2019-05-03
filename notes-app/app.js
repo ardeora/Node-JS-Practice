12d3790c5ce30d02b8e60e548dc940221038377a
Aryan Deora <adeora@iu.edu>
2019-05-03 03:14:41 -0400

    Section 3 Lecture 9: Importing Node Core Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,4 @@
+const fs = require('fs');
 
+// fs.writeFileSync('notes.txt', 'This file was created by node.js');
+fs.appendFileSync('notes.txt', 'This file was appended by the appendFileSync Function');
