f03718089b6dadf152e66d1605cc0f48862a12f9
Aryan Deora <adeora@iu.edu>
2019-05-04 18:27:16 -0400

    Section 3 Lecture 12: Importing Node Modules From NPM

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,7 @@
 const notes = require('./notes');
+const chalk = require('chalk');
 
+console.log(chalk.bold.black.bgGreen('Success!'));
+console.log(chalk.bold.rgb(15, 100, 204).inverse('Hello!'));
 console.log(notes.name);
 console.log(notes.add(2,3));
