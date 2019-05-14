edb7988a0f32fe37e913d1ab1e8af45a9fd17773
Aryan Deora <adeora@iu.edu>
2019-05-13 20:58:06 -0400

    Added Front End and Fetch API to Weather App

---
+++ web-server/public/weather/script.js
@@ -0,0 +1,160 @@
+const searchBtn = document.getElementById('search-btn');
+const addressBox = document.getElementById('address-box');
+const closeBtn = document.getElementById('close-add-btn');
+
+const day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+const weatherForm = document.querySelector('form');
+const searchBar = document.getElementById('inp-add');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Working!');
+    
+})
+fetch('http://localhost:3000/weather/?address=boston').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+        
+    })
+});
+
+searchBtn.addEventListener('click', () => {
+    addressBox.classList.remove('form-invisible');
+    addressBox.classList.add('form-visible');
+    const skyline = document.getElementById('SkyLine').style.fill;
+    addressBox.style.backgroundColor = skyline;
+    document.getElementById('submit-btn').style.color = skyline;
+});
+
+closeBtn.addEventListener('click', () => {
+    addressBox.classList.remove('form-visible');
+    addressBox.classList.add('form-invisible');
+});
+
+function setTime() {
+    let d = new Date();
+    let year = d.getFullYear().toString();
+    let str = `${day[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]} '${year.substring(2)}`;
+    document.getElementById('date').innerHTML = str;
+}
+setTime();
+
+
+function clearSvg() {
+    const assetGroup = document.querySelectorAll('#All-Assets > g, #All-Assets > path, #All-Assets > rect');
+    console.log(assetGroup);
+    assetGroup.forEach((asset) => {
+        asset.setAttribute('display', 'none')
+    });
+}
+
+function toggleDisplay(str) {
+    const assetGroup = document.querySelectorAll(str);
+    console.log(assetGroup);
+    assetGroup.forEach((asset) => {
+        asset.removeAttribute('display');
+    });
+    return assetGroup;
+}
+
+function changeIcon(str) {
+    let weatherIcon = document.getElementById('weather-icons');
+    weatherIcon.setAttribute('viewBox',str);
+}
+
+function changeSummary(str) {
+    let currentSummary = document.getElementById('current-summary');
+    currentSummary.innerHTML = str;
+}
+
+function setSVG(param) {
+    clearSvg();
+    if (param === 'clear-day') {
+        const assetStr = '#Sky, #SkyLine, #Dune2, #Dune1, #Sun';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--clear-day-sky)';
+        assetGroup[1].style.fill = 'var(--clear-day-skyline)';
+        assetGroup[2].style.fill = 'var(--clear-day-dune2)';
+        assetGroup[3].style.fill = 'var(--clear-day-dune1)';
+        changeIcon('0 0 50 50');  
+        changeSummary('Clear');   
+    } else if(param === 'clear-night') {
+        const assetStr = '#Sky, #Stars, #SkyLine, #Dune2, #Dune1, #Moon';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--clear-night-sky)';
+        assetGroup[2].style.fill = 'var(--clear-night-skyline)';
+        assetGroup[3].style.fill = 'var(--clear-night-dune2)';
+        assetGroup[4].style.fill = 'var(--clear-night-dune1)';
+        changeIcon('50 0 50 50');  
+        changeSummary('Clear'); 
+    } else if(param === 'cloudy' || param === 'partly-cloudy-day' || param === 'partly-cloudy-night') {
+        const assetStr = '#Sky, #Clouds, #SkyLine, #Dune2, #Dune1';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--cloudy-sky)';
+        assetGroup[2].style.fill = 'var(--cloudy-skyline)';
+        assetGroup[3].style.fill = 'var(--cloudy-dune2)';
+        assetGroup[4].style.fill = 'var(--cloudy-dune1)';
+        changeIcon('100 0 51 50');  
+        changeSummary('Cloudy');  
+    } else if(param === 'rain') {
+        const assetStr = '#Sky, #RainClouds, #SkyLine, #Dune2, #Dune1, #Rain';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--rain-sky)';
+        assetGroup[2].style.fill = 'var(--rain-skyline)';
+        assetGroup[3].style.fill = 'var(--rain-dune2)';
+        assetGroup[4].style.fill = 'var(--rain-dune1)'; 
+        changeIcon('151 0 51 50');  
+        changeSummary('Raining'); 
+    } else if(param === 'snow') {
+        const assetStr = '#Sky, #SkyLine, #Dune2, #IceSheet, #Dune1, #SnowMan, #SnowParticles';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--snow-sky)';
+        assetGroup[1].style.fill = 'var(--snow-skyline)';
+        assetGroup[2].style.fill = 'var(--snow-dune2)';
+        assetGroup[4].style.fill = 'var(--snow-dune1)';
+        changeIcon('201 0 51 50');  
+        changeSummary('Snowing'); 
+    }  else if(param === 'thunderstorm' || param === 'tornado') {
+        const assetStr = '#Sky, #ThunderClouds, #SkyLine, #Dune2, #Dune1, #ThunderRain';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--storm-sky)';
+        assetGroup[2].style.fill = 'var(--storm-skyline)';
+        assetGroup[3].style.fill = 'var(--storm-dune2)';
+        assetGroup[4].style.fill = 'var(--storm-dune1)';
+        changeIcon('251 0 51 50'); 
+        if (param === 'thunderstorm') {
+            changeSummary('Thunderstorm');  
+        } else {
+            changeSummary('Tornado'); 
+        }
+         
+    } else if(param === 'wind' || param === 'fog') {
+        const assetStr = '#Sky, #WindyClouds, #SkyLine, #Dune2, #Dune1';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--wind-sky)';
+        assetGroup[2].style.fill = 'var(--wind-skyline)';
+        assetGroup[3].style.fill = 'var(--wind-dune2)';
+        assetGroup[4].style.fill = 'var(--wind-dune1)';
+        changeIcon('302 0 50 50');
+        if (param === 'wind') {
+            changeSummary('Wind');  
+        } else {
+            changeSummary('Fog'); 
+        }           
+    } else if(param === 'hail' || param === 'sleet') {
+        const assetStr = '#Sky, #WindyClouds, #SkyLine, #Dune2, #Dune1, #SleetRain, #SleetSnow';
+        const assetGroup = toggleDisplay(assetStr);
+        assetGroup[0].style.fill = 'var(--sleet-sky)';
+        assetGroup[2].style.fill = 'var(--sleet-skyline)';
+        assetGroup[3].style.fill = 'var(--sleet-dune2)';
+        assetGroup[4].style.fill = 'var(--sleet-dune1)'; 
+        changeIcon('352 0 50 50');
+        if (param === 'hail') {
+            changeSummary('Hail');  
+        } else {
+            changeSummary('Sleet'); 
+        }
+    }
+}
+setSVG('clear-day');
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -18,10 +20,46 @@ hbs.registerPartials(partialsDir);
 app.use(express.static(publicDir));
 
 app.get('/weather', (req, res) => {
-    res.render('weather/index', {
-        title: 'Yoooooooooo This Bitch Working!'
+    if (!req.query.address) {
+        return res.render('weather/index', {
+            title: 'Yoooooooooo This Bitch Working!'
+        });
+    }
+
+    geocode(req.query.address, (error, geoData) => {
+        if (error) {
+            return res.send({
+                error: 'Mapbox Error: Could not retrieve coordinates of address'
+            })
+        } else {
+            forecast(geoData.latitude, geoData.longitude, (error, forData) => {
+                if (error) {
+                    res.send({
+                        error: 'Dark Sky Error: Could not retrieve forecast of address'
+                    })
+                } else {
+                    return res.send({
+                        forecast: forData,
+                        location: geoData.location
+                    })
+                }
+            })
+        }
+    })
+    // res.send({
+    //     error: 'Provide an address buddy',
+    // })
+    
+})
+
+app.get('/products', (req, res) => {
+    console.log(req.query);
+    
+    res.send({
+        products: []
     });
 })
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 })
